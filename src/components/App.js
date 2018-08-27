093866e4c4cd38a4d01679c9128f3f870f349716
jose.de.sousa <defmoves@gmail.com>
2018-08-27 13:52:56 +0100

    improved calandar

--- src/components/App.js
+++ src/components/App.js
@@ -1,65 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 import moment from "moment"
 import tz from "moment-timezone"
-import Weather from './weather/'
-import Calendar from './calendar/'
-import './App.css';
+import Weather from "./weather/"
+import Calendar from "./calendar/"
+import "./App.css"
 
-var dateFormat = require('dateformat');
-var MdTerrain = require('react-icons/lib/md/terrain');
-var MdWbSunny = require('react-icons/lib/md/wb-sunny');
+var dateFormat = require("dateformat")
+var MdTerrain = require("react-icons/lib/md/terrain")
+var MdWbSunny = require("react-icons/lib/md/wb-sunny")
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.ticker = this.ticker.bind(this)
+    this.state = { style: "day" }
+  }
 
-  constructor(props){
-          super(props);
-          this.ticker = this.ticker.bind(this)
-          this.state = {style : 'day'}
-    }
-
-  componentWillMount(){
+  componentWillMount() {
     this.ticker()
   }
 
-  ticker(){
+  ticker() {
     let self = this
     let date = new moment.tz("Europe/London")
-    let time = dateFormat(date, "HH : MM");
-    let timeNumeric = dateFormat(date, "HHMM");
-    let day = dateFormat(date, "dS mmmm");
-    let style = 'day'
+    let time = dateFormat(date, "HH:MM")
+    let timeNumeric = dateFormat(date, "HHMM")
+    let day = dateFormat(date, "dS mmmm")
+    let style = "day"
 
-    if (timeNumeric > 700 && timeNumeric < 2200 ){
-    //if (true ){
-      style = 'day'
+    if (timeNumeric > 700 && timeNumeric < 2200) {
+      //if (true ){
+      style = "day"
     } else {
-      style = 'night'
+      style = "night"
     }
 
     this.setState({
-      time : time,
-      day : day,
+      time: time,
+      day: day,
       style: style
     })
 
     setTimeout(() => {
       self.ticker()
-    },1000)
+    }, 1000)
   }
 
   render() {
     return (
       <div className="App">
-        <div className={'smartarse ' + this.state.style}>
+        <div className={"smartarse " + this.state.style}>
           <Weather />
-          <div className='clock'>
+          <div className="clock">
             <h1 className="time">{this.state.time}</h1>
           </div>
           <Calendar />
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
