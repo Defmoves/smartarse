f7f54c549800d524f6b96405ecb79e575f2812ed
jose.de.sousa <defmoves@gmail.com>
2018-09-18 20:01:28 +0100

    onwards

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -1,23 +1,20 @@
 import React, { Component } from "react"
 import Moment from "react-moment"
-import moment from "moment"
-import tz from "moment-timezone"
 import "./calendar.css"
 
-var dateFormat = require("dateformat")
-var Question = require("react-icons/lib/fa/plane")
+var Question = require("react-icons/lib/fa/question")
 var Heart = require("react-icons/lib/fa/heart")
-var Chain = require("react-icons/lib/fa/chain")
+var Group = require("react-icons/lib/fa/group")
 
 class App extends Component {
   render() {
     return (
       <div className="calendar">
         <div className="calendar-left">
-          <Question /> Sardinia <Moment fromNow>2018-09-05</Moment>
+          <Question /> Surprise <Moment fromNow>2018-09-26</Moment>
         </div>
         <div className="calendar-left">
-          <Heart /> Stinky <Moment fromNow>2018-09-12</Moment>
+          <Group /> D & Kev <Moment fromNow>2018-10-06</Moment>
         </div>
       </div>
     )
