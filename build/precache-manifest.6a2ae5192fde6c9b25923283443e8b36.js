d5ffed7ba4a5aeb2e037726659d5c218252a1d8e
jose.de.sousa <defmoves@gmail.com>
2018-10-28 16:27:50 +0000

    improved weather colour

---
+++ build/precache-manifest.6a2ae5192fde6c9b25923283443e8b36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2fea12f668645561a03e",
+    "url": "/static/js/main.2fea12f6.chunk.js"
+  },
+  {
+    "revision": "76f1477877a41a925239",
+    "url": "/static/js/1.76f14778.chunk.js"
+  },
+  {
+    "revision": "2fea12f668645561a03e",
+    "url": "/static/css/main.285512eb.chunk.css"
+  },
+  {
+    "revision": "3bba45ab8cc680ae817626be9965ebdc",
+    "url": "/index.html"
+  }
+];
