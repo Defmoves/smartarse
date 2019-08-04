290cdaa5e09a5f88c11f34981e7fa346015be6ae
defmoves <defmoves@pop-os.localdomain>
2019-08-04 16:55:45 +0100

    improved icons

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -2,17 +2,17 @@ import React, { Component } from "react";
 import Moment from "react-moment";
 import "./calendar.css";
 
-import { FaUmbrellaBeach, FaGift } from "react-icons/fa";
+import { FaPlaneDeparture, FaBirthdayCake } from "react-icons/fa";
 
 class App extends Component {
   render() {
     return (
       <div className="calendar">
         <div className="calendar-left">
-          <FaGift /> Stinky Bday <Moment fromNow>2019-09-24</Moment>
+          <FaBirthdayCake /> Stinky Bday <Moment fromNow>2019-09-24</Moment>
         </div>
         <div className="calendar-right">
-          <FaUmbrellaBeach /> Asia <Moment fromNow>2019-11-19</Moment>
+          <FaPlaneDeparture /> Asia <Moment fromNow>2019-11-19</Moment>
         </div>
       </div>
     );
