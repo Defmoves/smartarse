0916ec59c7fcb6afc322a131aa9981a4c12045f4
defmoves <defmoves@pop-os.localdomain>
2019-09-24 19:42:41 +0100

    added holiday

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -2,27 +2,32 @@ import React, { Component } from "react";
 import Moment from "react-moment";
 import "./calendar.css";
 
-import { FaFilm, FaBook, FaFeatherAlt, FaGlobeEurope } from "react-icons/fa";
+import {
+  FaUmbrellaBeach,
+  FaBook,
+  FaFeatherAlt,
+  FaGlobeEurope
+} from "react-icons/fa";
 
 class App extends Component {
   render() {
     return (
       <div className="calendar">
         <div className="calendar-left">
-          <FaFilm /> Brixton Ritzy&nbsp;
-          <Moment fromNow>2019-09-23T00:00</Moment>
-        </div>
-        <div className="calendar-right">
           Uninhabitable <FaGlobeEurope />
           <Moment fromNow>2019-10-02T00:00</Moment>
         </div>
-        <div className="calendar-left">
+        <div className="calendar-right">
           <FaBook /> Naomi Klein&nbsp;
           <Moment fromNow>2019-10-15T00:00</Moment>
         </div>
-        <div className="calendar-right">
+        <div className="calendar-left">
           <FaFeatherAlt /> Neil Gaiman&nbsp;
           <Moment fromNow>2019-11-14T00:00</Moment>
+        </div>
+        <div className="calendar-right">
+          <FaUmbrellaBeach /> Holiday begins&nbsp;
+          <Moment fromNow>2019-11-15T17:00</Moment>
         </div>
       </div>
     );
