a5deb2f8ba0905415b07d80d3f7b4a7fd6b03152
defmoves <defmoves@pop-os.localdomain>
2019-10-07 19:16:01 +0100

    added me birthday

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -2,32 +2,28 @@ import React, { Component } from "react";
 import Moment from "react-moment";
 import "./calendar.css";
 
-import {
-  FaUmbrellaBeach,
-  FaBook,
-  FaFeatherAlt,
-  FaGlobeEurope
-} from "react-icons/fa";
+import { FaUmbrellaBeach, FaBook, FaFeatherAlt, FaGift } from "react-icons/fa";
 
 class App extends Component {
   render() {
     return (
       <div className="calendar">
         <div className="calendar-left">
-          Uninhabitable <FaGlobeEurope />
-          <Moment fromNow>2019-10-02T00:00</Moment>
+          <FaBook /> Naomi Klein&nbsp;
+          <Moment fromNow>2019-10-15T20:00</Moment>
         </div>
         <div className="calendar-right">
-          <FaBook /> Naomi Klein&nbsp;
-          <Moment fromNow>2019-10-15T00:00</Moment>
+          <FaFeatherAlt /> Neil Gaiman&nbsp;
+          <Moment fromNow>2019-11-14T19:00</Moment>
         </div>
         <div className="calendar-left">
-          <FaFeatherAlt /> Neil Gaiman&nbsp;
-          <Moment fromNow>2019-11-14T00:00</Moment>
-        </div>
-        <div className="calendar-right">
           <FaUmbrellaBeach /> Holiday begins&nbsp;
           <Moment fromNow>2019-11-15T17:00</Moment>
+        </div>
+        <div className="calendar-right">
+          <FaGift />
+          José's birthday&nbsp;
+          <Moment fromNow>2019-11-23T00:00</Moment>
         </div>
       </div>
     );
