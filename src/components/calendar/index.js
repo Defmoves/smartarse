53c56b62975d04fbb8700cff18dd267810f43e08
defmoves <defmoves@pop-os.localdomain>
2019-10-20 13:08:45 +0100

    calandar update

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -9,21 +9,22 @@ class App extends Component {
     return (
       <div className="calendar">
         <div className="calendar-left">
-          <FaBook /> Naomi Klein&nbsp;
-          <Moment fromNow>2019-10-15T20:00</Moment>
-        </div>
-        <div className="calendar-right">
           <FaFeatherAlt /> Neil Gaiman&nbsp;
           <Moment fromNow>2019-11-14T19:00</Moment>
         </div>
-        <div className="calendar-left">
+        <div className="calendar-right">
           <FaUmbrellaBeach /> Holiday begins&nbsp;
           <Moment fromNow>2019-11-15T17:00</Moment>
         </div>
-        <div className="calendar-right">
+        <div className="calendar-left">
           <FaGift />
           José's birthday&nbsp;
           <Moment fromNow>2019-11-23T00:00</Moment>
+        </div>
+        <div className="calendar-right">
+        <FaGift />
+          Cat's birthday&nbsp;
+          <Moment fromNow>2019-12-08T00:00</Moment>
         </div>
       </div>
     );
