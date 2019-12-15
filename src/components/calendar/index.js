7bdc36ad1d0773cf78a2cdcfaa2382d667292db1
defmoves <defmoves@pop-os.localdomain>
2019-12-15 19:04:55 +0000

    updated calandar

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -2,28 +2,28 @@ import React, { Component } from "react";
 import Moment from "react-moment";
 import "./calendar.css";
 
-import { FaUmbrellaBeach, FaBook, FaFeatherAlt, FaGift } from "react-icons/fa";
+import { FaGift, FaPlaneDeparture, FaGlassCheers } from "react-icons/fa";
 
 class App extends Component {
   render() {
     return (
       <div className="calendar">
         <div className="calendar-left">
-          <FaBook /> Naomi Klein&nbsp;
-          <Moment fromNow>2019-10-15T20:00</Moment>
+          <FaPlaneDeparture /> Benny flies to Bologna&nbsp;
+          <Moment fromNow>2019-12-21T20:00</Moment>
         </div>
         <div className="calendar-right">
-          <FaFeatherAlt /> Neil Gaiman&nbsp;
-          <Moment fromNow>2019-11-14T19:00</Moment>
+          <FaGift /> Christmas Day&nbsp;
+          <Moment fromNow>2019-12-25T19:00</Moment>
         </div>
         <div className="calendar-left">
-          <FaUmbrellaBeach /> Holiday begins&nbsp;
-          <Moment fromNow>2019-11-15T17:00</Moment>
+          <FaPlaneDeparture /> José flies to Bologna&nbsp;
+          <Moment fromNow>2019-12-28T17:00</Moment>
         </div>
         <div className="calendar-right">
-          <FaGift />
-          José's birthday&nbsp;
-          <Moment fromNow>2019-11-23T00:00</Moment>
+          <FaGlassCheers />
+          New Years Day &nbsp;
+          <Moment fromNow>2020-01-01T00:00</Moment>
         </div>
       </div>
     );
