aa7bf1d6cb8e9363f905d3f51969cde216e4cb71
José Des Sousa <defmoves@pm.me>
2020-02-03 22:21:44 +0000

    new dates

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -2,28 +2,27 @@ import React, { Component } from "react";
 import Moment from "react-moment";
 import "./calendar.css";
 
-import { FaGift, FaPlaneDeparture, FaGlassCheers } from "react-icons/fa";
+import { FaGift, FaPlaneDeparture, FaBaby } from "react-icons/fa";
 
 class App extends Component {
   render() {
     return (
       <div className="calendar">
         <div className="calendar-left">
-          <FaPlaneDeparture /> Benny flies to Bologna&nbsp;
-          <Moment fromNow>2019-12-21T20:00</Moment>
-        </div>
-        <div className="calendar-right">
-          <FaGift /> Christmas Day&nbsp;
-          <Moment fromNow>2019-12-25T19:00</Moment>
+          <FaPlaneDeparture /> Benny in Bologna&nbsp;
+          <Moment fromNow>2020-02-07T20:00</Moment>
         </div>
         <div className="calendar-left">
-          <FaPlaneDeparture /> José flies to Bologna&nbsp;
-          <Moment fromNow>2019-12-28T17:00</Moment>
+          <FaPlaneDeparture /> Benny comes home&nbsp;
+          <Moment fromNow>2020-02-10T20:00</Moment>
         </div>
-        <div className="calendar-right">
-          <FaGlassCheers />
-          New Years Day &nbsp;
-          <Moment fromNow>2020-01-01T00:00</Moment>
+        <div className="calendar-left">
+          <FaBaby /> Baby Scan&nbsp;
+          <Moment fromNow>2020-02-11T20:00</Moment>
+        </div>
+        <div className="calendar-left">
+          <FaBaby /> The Baby Show&nbsp;
+          <Moment fromNow>2020-03-01T09:00</Moment>
         </div>
       </div>
     );
