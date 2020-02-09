7a17a938fe39ebaf2715acec4c580f602cae9b03
José Des Sousa <defmoves@pm.me>
2020-02-09 22:37:02 +0000

    updated reminders

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -2,25 +2,25 @@ import React, { Component } from "react";
 import Moment from "react-moment";
 import "./calendar.css";
 
-import { FaPlaneDeparture, FaPlaneArrival, FaBaby } from "react-icons/fa";
+import { FaBaby } from "react-icons/fa";
 
 class App extends Component {
   render() {
     return (
       <div className="calendar">
         <div className="calendar-left">
-          <FaPlaneDeparture /> Benny in Bologna&nbsp;
-          <Moment fromNow>2020-02-07T20:00</Moment>
-        </div>
-        <div className="calendar-left">
-          <FaPlaneArrival /> Benny comes home&nbsp;
-          <Moment fromNow>2020-02-10T20:00</Moment>
-        </div>
-        <div className="calendar-left">
           <FaBaby /> Baby Scan&nbsp;
           <Moment fromNow>2020-02-11T20:00</Moment>
         </div>
+        <div className="calendar-right">
+          <FaBaby /> Meet the Midwife&nbsp;
+          <Moment fromNow>2020-02-13T20:00</Moment>
+        </div>
         <div className="calendar-left">
+          <FaBaby /> Nursery Tour&nbsp;
+          <Moment fromNow>2020-02-14T14:30</Moment>
+        </div>
+        <div className="calendar-right">
           <FaBaby /> The Baby Show&nbsp;
           <Moment fromNow>2020-03-01T09:00</Moment>
         </div>
