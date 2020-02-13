5cf292f6be92a56c7ec57808eb9ac30aca126c6f
defmoves <defmoves@dionysus.localdomain>
2020-02-13 22:22:04 +0000

    updated calandar

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -2,23 +2,15 @@ import React, { Component } from "react";
 import Moment from "react-moment";
 import "./calendar.css";
 
-import { FaBaby } from "react-icons/fa";
+import { FaBaby, FaSchool } from "react-icons/fa";
 
 class App extends Component {
   render() {
     return (
       <div className="calendar">
         <div className="calendar-left">
-          <FaBaby /> Baby Scan&nbsp;
-          <Moment fromNow>2020-02-11T20:00</Moment>
-        </div>
-        <div className="calendar-right">
-          <FaBaby /> Meet the Midwife&nbsp;
-          <Moment fromNow>2020-02-13T20:00</Moment>
-        </div>
-        <div className="calendar-left">
-          <FaBaby /> Nursery Tour&nbsp;
-          <Moment fromNow>2020-02-14T14:30</Moment>
+          <FaSchool /> NCT Begins&nbsp;
+          <Moment fromNow>2020-02-27T19:00</Moment>
         </div>
         <div className="calendar-right">
           <FaBaby /> The Baby Show&nbsp;
