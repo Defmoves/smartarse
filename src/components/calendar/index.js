f57ffc2633697501931845f134a3d27a6aacfd67
defmoves <defmoves@dionysus.localdomain>
2020-04-21 00:42:36 +0100

    a wee facelift

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -2,18 +2,14 @@ import React from "react";
 import Moment from "react-moment";
 import "./calendar.css";
 
-import { FaBaby, FaPlaneArrival } from "react-icons/fa";
+import { FaBaby } from "react-icons/fa";
 
 const App = () => {
   return (
     <div className="calendar">
-      <div className="calendar-left">
-        <FaPlaneArrival /> Stinky arrives&nbsp;
-        <Moment fromNow>2020-04-02T14:00</Moment>
-      </div>
-      <div className="calendar-right">
-        <FaBaby /> Dinky Due&nbsp;
-        <Moment fromNow>2020-05-02T13:00</Moment>
+      <div className="calendar-center">
+        <FaBaby /> Tiny little Dinky Due&nbsp;
+        <Moment fromNow>2020-05-02T13:00</Moment>!
       </div>
     </div>
   );
