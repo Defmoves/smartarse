766cc6b6478cfc17cf825434456676a20c3e9354
defmoves <defmoves@pm.me>
2021-07-27 20:37:47 +0100

    simplification

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -2,17 +2,14 @@ import React from "react";
 import { formatDistanceToNow } from 'date-fns'
 import "./calendar.css"
 
-import { FaClinicMedical } from "react-icons/fa";
+import { FaWineBottle } from "react-icons/fa";
 
 const App = () => {
   return (
     <div className="calendar">
       <div className="calendar-center">
-        <FaClinicMedical />José Vaccine in&nbsp;
-        {formatDistanceToNow(new Date(2021, 6, 24, 12, 0))}</div>
-        <div className="calendar-center">
-        <FaClinicMedical />Benny Vaccine in&nbsp;
-        {formatDistanceToNow(new Date(2021, 6, 25, 12, 0))}</div>
+        <FaWineBottle />Sergio Coming in&nbsp;
+        {formatDistanceToNow(new Date("August 7, 2021"))}</div>
     </div>
   );
 };
