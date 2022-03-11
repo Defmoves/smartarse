b21f44fd75aefca0c615807902ed487a1ac60904
defmoves <defmoves@pm.me>
2022-03-11 20:32:37 +0000

    stinky birthday

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { formatDistanceToNow } from "date-fns";
 import "./calendar.css";
 
-import { FaPlaneDeparture } from "react-icons/fa";
+import { FaGift } from "react-icons/fa";
 
 const App = () => {
   const [date, setDate] = useState("?");
 
   useEffect(() => {
     const ticker = () => {
-      const date = formatDistanceToNow(new Date("February 19, 2022"));
+      const date = formatDistanceToNow(new Date("May 4, 2022"));
       setDate(date);
     };
     setInterval(() => ticker(), 1800000); // every .5 hours
@@ -19,8 +19,8 @@ const App = () => {
   return (
     <div className="calendar">
       <div className="calendar-center">
-        <FaPlaneDeparture />
-        Bologna in &nbsp;
+        <FaGift />
+        Ethan's birthday in&nbsp;
         {date}!
       </div>
     </div>
