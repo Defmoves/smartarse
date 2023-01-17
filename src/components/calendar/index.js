da5823a864a947a308f13791ac38f4d064b370f5
defmoves <defmoves@pm.me>
2023-01-17 23:20:49 +0000

    updated calendar

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { formatDistanceToNow } from "date-fns";
 import "./calendar.css";
 
-import { FaSkating } from "react-icons/fa";
+import { GiPartyPopper } from "react-icons/gi";
 
 const App = () => {
   const [date, setDate] = useState("?");
 
   useEffect(() => {
     const ticker = () => {
-      const date = formatDistanceToNow(new Date("January 12, 2023"));
+      const date = formatDistanceToNow(new Date("January 19, 2023"));
       setDate(date);
     };
     setInterval(() => ticker(), 1800000); // every .5 hours
@@ -19,8 +19,8 @@ const App = () => {
   return (
     <div className="calendar">
       <div className="calendar-center">
-        <FaSkating />
-         Sergio arrives in &nbsp;
+        <GiPartyPopper />
+         Alma arrives in &nbsp;
         {date}!
       </div>
     </div>
