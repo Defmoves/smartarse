aab355c334e2ab3f08dcde120f7b47a5a5269800
defmoves <defmoves@pm.me>
2023-09-18 18:17:27 +0100

    calendar update

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { formatDistanceToNow } from "date-fns";
 import "./calendar.css";
 
-import { FaPlaneDeparture } from "react-icons/fa";
+import { FaBirthdayCake } from "react-icons/fa";
 
 const App = () => {
   const [date, setDate] = useState("?");
 
   useEffect(() => {
     const ticker = () => {
-      const date = formatDistanceToNow(new Date("August 25, 2023"));
+      const date = formatDistanceToNow(new Date("November 23, 2023"));
       setDate(date);
     };
     setInterval(() => ticker(), 1800000); // every .5 hours
@@ -19,8 +19,8 @@ const App = () => {
   return (
     <div className="calendar">
       <div className="calendar-center">
-        <FaPlaneDeparture />
-          to cles in {date}!
+        <FaBirthdayCake />
+          Old man gets even older in {date}!
       </div>
     </div>
   );
